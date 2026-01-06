a735157f01eeb93b39828a7f9b2a0ce452ba116a
Daniel Reis <kr.danielreis@gmail.com>
2026-01-05 22:57:13 -0300

    Adicionando parametros

--- cypress/e2e/buscar_dispositivo.cy.js
+++ cypress/e2e/buscar_dispositivo.cy.js
@@ -1,9 +1,13 @@
 <reference types="cypress" />
 
 describe('Buscar Dispositivo', () => {
-    it ('Deve buscar um dispositivo existente', () => {
-        cypress.request('GET', 
+    it('Deve buscar um dispositivo existente', () => {
+        cypress.request('GET',
             url, '/objects/3', {
+        }).then((resposta) => {
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.id).to.have.property('id', 3);
+            expect(resposta.body.name).to.have.property('name', 'Aple iPhone 12 Pro Max');
         });
     });
 });
