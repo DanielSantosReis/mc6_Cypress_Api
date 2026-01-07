418d581a0d5b75c642a2c01c55a7999e941614b0
Daniel Reis <kr.danielreis@gmail.com>
2026-01-06 23:33:55 -0300

    Adicionando parametros

--- cypress/e2e/cadastrar_dispositivo.cy.js
+++ cypress/e2e/cadastrar_dispositivo.cy.js
@@ -1,9 +1,30 @@
+import { expect } from "chai";
+
 <reference types="cypress" />
 
-describe('Cadastro de Dispositivo', () => {
-    it ('Deve cadastrar um novo dispositivo com sucesso', () => {
-        cypress.request('GET', 
-            url, 'https://api.restfull-api.dev/objects/3', {
-        });
+describe('Cadastrar Dispositivo', () => {
+    it('Deve cadastrar um novo dispositivo', () => {
+
+        const dado = {
+                    data: {
+                    year: 2020,
+                    price: 1099.99,
+                    cpu: 'Apple A14 Bionic',
+                    camera: '12MP'
+                },
+        }
+
+        cy.request({
+            method:"POST",
+            url: '/objects',
+            body: dado
+
+        })
+            .then((resposta) => {
+                expect(resposta.status).to.equal(200);
+                expect(resposta.body.id).not.to.empty;
+                expect(resposta.body.createdAt).not.to.empty;
+                expect(resposta.body.name).to.equal('Apple iPhone 12 Pro Max');
+             });
     });
 });
