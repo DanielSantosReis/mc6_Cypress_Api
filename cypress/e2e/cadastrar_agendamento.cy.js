04c129a15feedbbd9439fe600c8163b7ae43a451
Daniel Reis <kr.danielreis@gmail.com>
2026-01-11 22:05:41 -0300

    Atualizando cadastro de agendamento

--- cypress/e2e/cadastrar_agendamento.cy.js
+++ cypress/e2e/cadastrar_agendamento.cy.js
@@ -6,14 +6,14 @@ describe("Cadastrar Reservas", () => {
     it("Deve cadastrar uma reserva com sucesso", () => {
 
         const payload = {
-           "firstname": "Daniel",
-           "lastname": "Reis",
-           "totalprice": 400,
-           "depositpaid": true,
-              "bookingdates": {
+            "firstname": "Daniel",
+            "lastname": "Reis",
+            "totalprice": 400,
+            "depositpaid": true,
+            "bookingdates": {
                 "checkin": "2024-01-01",
                 "checkout": "2024-01-05"
-              },
+            },
             "additionalneeds": "Breakfast",
         }
         cy.request({
@@ -24,6 +24,14 @@ describe("Cadastrar Reservas", () => {
         })
             .then((resposta) => {
                 expect(resposta.status).to.equal(200);
-            });
-    });
+                expect(resposta.body.bookingid).not.be.NaN;
+                expect(resposta.body.booking.firstname).to.equal("Daniel");
+                expect(resposta.body.booking.lastname).to.equal("Reis");
+                expect(resposta.body.booking.totalprice).to.equal(400);
+                expect(resposta.body.booking.depositpaid).to.equal(true);
+                expect(resposta.body.booking.bookingdates.checkin).to.equal("2024-01-01");
+                expect(resposta.body.booking.bookingdates.checkout).to.equal("2024-01-05");
+                expect(resposta.body.booking.additionalneeds).to.equal("Breakfast");
+        });
+     });
 });
